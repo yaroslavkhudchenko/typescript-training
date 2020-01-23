329e14ed16a92bff24090fdad73129a99313def8
Lune <yari.lune@gmail.com>
2020-01-23 10:14:36 +0100

    add example variables and function

---
+++ js/main.ts
@@ -0,0 +1,14 @@
+let ex: (number | boolean)[] = [1,2,3,4,5, true]; // number and boolean
+let exAny: any = [1,'sfa',true]; // allow all
+
+// ex = ''; // cannot change type
+
+/* let ex2 = ex.map((o)=>{
+    return o + 1;
+}) */ // cannot because of type restrictions
+
+
+function add(num1: number, num2: number) : string {// expect to return a number
+    // return num1 + num2; // cannot type string is not assignable to type string
+    return (num1 + num2).toString(); // correct
+}
